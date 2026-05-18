1302b68ce8766d51ef1ea02be95f1cf7adc1ed57
riririnn <305151a@gs.kochi-tech.ac.jp>
2026-05-18 15:54:43 +0900

    Update CMakeLists.txt to find Python and pybind11 components; modify start-container.sh to comment out old image command

--- scripts/start-container.sh
+++ scripts/start-container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
-./minizero/scripts/start-container.sh --image yanrudocker/restnet-go $@
+# ./minizero/scripts/start-container.sh --image yanrudocker/restnet-go $@
+./minizero/scripts/start-container-gpu.sh --image myanrudocker/restnet-go $@
